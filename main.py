f6cd11ac0825efe5ed248edc80aad8e1dd53289e
liuzy <lzygzh@126.com>
2019-08-20 16:52:52 +0800

    change

---
+++ main.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#####################################
+# File name : main.py
+# Create date : 2019-08-20 15:51
+# Modified date : 2019-08-20 16:35
+# Author : DARREN
+# Describe : not set
+# Email : lzygzh@126.com
+#####################################
+from __future__ import division
+from __future__ import print_function
+
+from sim_cilin import SimCilin
+from sim_hownet import SimHownet
+from sim_simhash import SimHaming
+from sim_tokenvector import SimTokenVec
+from sim_vsm import SimVsm
+
+def test_text(text1, text2):
+    print("sentence1:%s" % text1)
+    print("sentence2:%s" % text2)
+
+    cilin = SimCilin()
+    print('cilin', cilin.distance(text1, text2))
+
+    hownet = SimHownet()
+    print('hownet', hownet.distance(text1, text2))
+
+    simhash = SimHaming()
+    print('simhash', simhash.distance(text1, text2))
+
+    simtoken = SimTokenVec()
+    print('simtoken', simtoken.distance(text1, text2))
+
+    simvsm = SimVsm()
+    print('simvsm', simvsm.distance(text1, text2))
+    print('\n')
+
+def test():
+    cilin = SimCilin()
+    hownet = SimHownet()
+    simhash = SimHaming()
+    simtoken = SimTokenVec()
+    simvsm = SimVsm()
+
+    while 1:
+        text1 = input('enter sent1:').strip()
+        text2 = input('enter sent2:').strip()
+        print('cilin', cilin.distance(text1, text2))
+        print('hownet', hownet.distance(text1, text2))
+        print('simhash', simhash.distance(text1, text2))
+        print('simtoken', simtoken.distance(text1, text2))
+        print('simvsm', simvsm.distance(text1, text2))
+
+def run():
+    text1 = "南昌是江西省的省会"
+    text2 = "北京乃中国之首都"
+    test_text(text1, text2)
+
+    text1 = "我是中国人，我深爱着我的祖国"
+    text2 = "中国是我的母亲，我热爱她"
+    test_text(text1, text2)
+
+    text1 = "一群高贵气质的差人在处罚违章动物"
+    text2 = "城管执法，若不文明会导致很多不公平事故"
+    test_text(text1, text2)
+
+    text1 = "小明去了姥姥家，姥姥给他买了一本童话书"
+    text2 = "我外婆早早的就出去了，给我带回来一本恐怖小说"
+    test_text(text1, text2)
+
+    #test()
+
+run()
---
+++ sim_simhash.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#####################################
+# File name : sim_simhash.py
+# Create date : 2019-08-20 16:31
+# Modified date : 2019-08-20 16:35
+# Author : DARREN
+# Describe : not set
+# Email : lzygzh@126.com
+#####################################
+from __future__ import division
+from __future__ import print_function
+
+from simhash import Simhash
+import jieba.posseg as pseg
+
+class SimHaming:
+
+    '''利用64位数，计算海明距离'''
+    def haming_distance(self, code_s1, code_s2):
+        x = (code_s1 ^ code_s2) & ((1 << 64) - 1)
+        ans = 0
+        while x:
+            ans += 1
+            x &= x - 1
+        return ans
+
+    '''利用相似度计算方式,计算全文编码相似度'''
+    def get_similarity(self, a, b):
+        if a > b :
+            return b / a
+        else:
+            return a / b
+
+    '''对全文进行分词,提取全文特征,使用词性将虚词等无关字符去重'''
+    def get_features(self, string):
+        word_list=[word.word for word in pseg.cut(string) if word.flag[0] not in ['u','x','w','o','p','c','m','q']]
+        return word_list
+
+    '''计算两个全文编码的距离'''
+    def get_distance(self, code_s1, code_s2):
+        return self.haming_distance(code_s1, code_s2)
+
+    '''对全文进行编码'''
+    def get_code(self, string):
+        return Simhash(self.get_features(string)).value
+
+    '''计算s1与s2之间的距离'''
+    def distance(self, s1, s2):
+        code_s1 = self.get_code(s1)
+        code_s2 = self.get_code(s2)
+        similarity = (100 - self.haming_distance(code_s1,code_s2)*100/64)/100
+        return similarity
+
+def test():
+    text1 = '我喜欢你'
+    text2 = '我讨厌你'
+    simer = SimHaming()
+    sim = simer.distance(text1, text2)
+    print(sim)
+
+#test()
